99119988b9dde6051b53609fdb6d8fbd67e3d1d4
Hans Trautlein <hans@trautlein.com>
2016-10-18 15:31:13 -0700

    Server seems to be in great shape.

--- server/app.js
+++ server/app.js
@@ -1,41 +1,37 @@
 const express = require('express');
 const Promise = require('bluebird');
-
 const DarkSky = require('dark-sky');
 const forecast = new DarkSky('8b8f3aa22900a0803b951d23511a5d2b');
-
 // Middleware
+const cors = require('cors');
 const morgan = require('morgan');
 const parser = require('body-parser');
-
-// const router = require('./routes.js');
-
 const app = express();
 app.set('port', 1337);
 
 // Logging and parsing
 app.use(morgan('dev'));
 app.use(parser.json());
+app.use(cors());
 
-// Set up our routes
-// app.use('/classes', router);
-
-// Serve the client files
 
 app.listen(app.get('port'));
 console.log('Listening on', app.get('port'));
 
-app.get('/', function(req, res) {
+app.post('/api', function (req, res) {
+  let coords = req.body;
+
   forecast
-    .latitude(37.7860)
-    .longitude(-122.4071)
+    .latitude(coords.lat)
+    .longitude(coords.long)
     .units('us')
     .get()
     .then( data => {
-      res.send(data.currently);
+      res.send(JSON.stringify(data));
       res.end();
     })
     .catch(err => {
-      console.log(err)
+      console.log(err);
+      res.end('Sorry, your forecast didn\'t go through. Please try checking your latitude and longitude points.');
     });
 });
